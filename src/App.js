6808709b5aa6d28a8ae2515f62f8d8e639080fba
Jonathan Dussot <jonathandussot@gmail.com>
2024-10-18 11:30:04 +0000

    Created ProfilePage.js with stylesheet for user's profile.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import GamePage from "./pages/games/GamePage";
 import GamesPage from "./pages/games/GamesPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import GameEditForm from "./pages/games/GameEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -42,6 +43,7 @@ function App() {
           <Route exact path="/games/create" render={() => <GameCreateForm />} />
           <Route exact path="/games/:id" render={() => <GamePage />} />
           <Route exact path="/games/:id/edit" render={() => <GameEditForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
