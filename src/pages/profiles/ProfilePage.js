aaab08fde7129f2b2c4eceec421f1d4e6545ac98
Jonathan Dussot <jonathandussot@gmail.com>
2024-10-19 13:28:07 +0000

    Removed edit button in ProfilePage.

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -4,7 +4,6 @@ import Col from "react-bootstrap/Col"; // Import Col for layout
 import logo from "../../assets/logo.png";
 import styles from '../../styles/ProfilePage.module.css';
 import appStyles from '../../App.module.css';
-import btnStyles from '../../styles/Button.module.css';
 import { useParams } from 'react-router-dom';
 import { axiosReq } from '../../api/axiosDefaults';
 import { Image, Card } from 'react-bootstrap';
@@ -54,9 +53,6 @@ const ProfilePage = () => {
             <Card.Text className={`${styles.ProfileInfo} text-muted`}>
               <strong>Games Posted:</strong> {profileData.games_count}
             </Card.Text>
-            <button className={`${btnStyles.Button} ${btnStyles.EditButton} mt-3`}>
-              Edit Profile
-            </button>
           </Card.Body>
         </Card>
       ) : (
