04cf39ec6de3c6ecb2520d4f6464006a864cfa50
Jonathan Dussot <jonathandussot@gmail.com>
2024-10-19 09:45:41 +0000

    Added code to ProfileDataContext and created GameDataContext.js file.

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    const [profileData, setProfileData] = useState({
+        // we will use the pageProfile later!
+        pageProfile: { results: [] },
+    });
+
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        if (currentUser) {
+            const fetchProfile = async () => {
+                try {
+                    const { data } = await axiosReq.get(`/profiles/${currentUser.profile_id}/`);
+                    setProfileData({ pageProfile: data });
+                } catch (err) {
+                    console.log(err);
+                }
+            };
+
+            fetchProfile();
+        }
+    }, [currentUser]);
+
+    return (
+        <ProfileDataContext.Provider value={profileData}>
+            <SetProfileDataContext.Provider value={setProfileData}>
+                {children}
+            </SetProfileDataContext.Provider>
+        </ProfileDataContext.Provider>
+    );
+};
